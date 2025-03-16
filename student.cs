0ccabc6545b09337ec64a8566cdc0fc0bb7beb1d
unknown <you@example.com>
2025-03-16 10:33:43 -0700

    Class: a)GetInfo hazirdir

---
+++ student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask17marchclass
+{
+    class Student
+    { public string Name {  get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public float Point { get; set; }
+        public bool isGraduated { get; set; }
+
+        public Student(string name, string surname, string group, float point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+        }
+        
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
